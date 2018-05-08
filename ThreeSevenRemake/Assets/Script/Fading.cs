866ab1020ba59bc0bab26cb225ae7365db8e6ff2
GaryAu83GitHub <gary.au.83.ga@gmail.com>
2018-05-08 14:37:32 +0200

    GUI upgrade
    TextMesh pro is added to the project in purpose for making better GUI text for the game.
    
    A basic start meny is added and a simple fade in between sceen is in active.

---
+++ ThreeSevenRemake/Assets/Script/Fading.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fading : MonoBehaviour
+{
+    public Texture2D FadeOutTexture;
+    public float FadeSpeed = .8f;
+
+    private int mDrawDepth = -1000;
+    private float mAlpha = 1f;
+    private int mFadeDir = -1;
+
+    private void OnGUI()
+    {
+        mAlpha += mFadeDir * FadeSpeed * Time.deltaTime;
+        mAlpha = Mathf.Clamp01(mAlpha);
+
+        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, mAlpha);
+        GUI.depth = mDrawDepth;
+        GUI.DrawTexture(new Rect(-(Screen.width / 2), -(Screen.height / 2), Screen.width * 2, Screen.height * 2), FadeOutTexture);
+    }
+
+    public float BeginFade(int aDirection)
+    {
+        mFadeDir = aDirection;
+        return FadeSpeed;
+    }
+
+    void OnLevelWasLoaded()
+    {
+        BeginFade(-1);
+    }
+}
---
+++ ThreeSevenRemake/Assets/Script/MainMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Fading FadingScript;
+
+    public Button PlayButton;
+    public Button InstructionButton;
+    public Button QuitButton;
+
+    public GameObject InstructionPanel;
+
+    private void Start()
+    {
+        PlayButton.onClick.AddListener(PlayGame);
+        InstructionButton.onClick.AddListener(InstructingGame);
+        QuitButton.onClick.AddListener(QuitGame);
+    }
+
+    public void PlayGame()
+    {
+        FadingScript.gameObject.SetActive(true);
+        StartCoroutine(GoToGame());
+    }
+
+    public void InstructingGame()
+    {
+        InstructionPanel.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void InstructingOut()
+    {
+        InstructionPanel.SetActive(false);
+    }
+
+    IEnumerator GoToGame()
+    {
+        float fadeTime = FadingScript.BeginFade(1);
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadScene("Main");
+    }
+}
